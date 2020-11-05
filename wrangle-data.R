63b4a57cd08c07dc421072850980587d5c7e1230
Odalys Jordan <odalys.jordan@gmail.com>
2020-11-04 22:33:08 -0500

    Initial commit. All files added.

---
+++ wrangle-data.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+murders <- read.csv("data/murders.csv")
+head(murders, 10)
+murders <- murders %>% mutate(region2 = factor(region), rate = total / population * 10^5)
+save(murders, file = "rda/murders.rda")
